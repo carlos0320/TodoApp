613f9a9155f051d6196c442fda34a52a5abe2778
Carlos Grisales <41649792+carlos0320@users.noreply.github.com>
2021-07-06 07:14:38 -0500

    repo initialized

---
+++ docs/precache-manifest.3b1ed19b638410981c51b2375dcf7dc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def4f7573147704c56b5f72d1b1c637f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fe9467e4689930f02d2",
+    "url": "/static/css/main.770d8125.chunk.css"
+  },
+  {
+    "revision": "fe8334f92ceb5a1da53d",
+    "url": "/static/js/2.eb9b3ff2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eb9b3ff2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fe9467e4689930f02d2",
+    "url": "/static/js/main.0a4bb608.chunk.js"
+  },
+  {
+    "revision": "af7dc8a15bdbc8d02f91",
+    "url": "/static/js/runtime-main.50f8c248.js"
+  }
+]);
